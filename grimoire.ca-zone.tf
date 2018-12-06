ec997e90cc0bfcc8fe175193b50d634b88a834e3
Owen Jacobson <owen@grimoire.ca>
2018-12-05 20:21:18 -0500

    Restructured Terraform files to allow multiple zones.

--- grimoire.ca-zone.tf
+++ grimoire.ca-zone.tf
@@ -1,17 +1,3 @@
-terraform {
-  backend "s3" {
-    bucket = "terraform.grimoire"
-    key    = "dns.tfstate"
-    region = "ca-central-1"
-  }
-}
-
-provider "aws" {
-  version = "~> 1.11"
-
-  region = "ca-central-1"
-}
-
 resource "aws_route53_zone" "primary" {
   name = "grimoire.ca"
 }
---
+++ terraform.tf
@@ -0,0 +1,13 @@
+terraform {
+  backend "s3" {
+    bucket = "terraform.grimoire"
+    key    = "dns.tfstate"
+    region = "ca-central-1"
+  }
+}
+
+provider "aws" {
+  version = "~> 1.11"
+
+  region = "ca-central-1"
+}
