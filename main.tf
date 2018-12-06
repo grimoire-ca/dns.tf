c4f60b1c7c6e469e512b4de85ae847096d51b87d
Owen Jacobson <owen@grimoire.ca>
2018-12-05 22:04:03 -0500

    Add per-zone outputs.

--- main.tf
+++ main.tf
@@ -16,10 +16,20 @@ module "grimoire_ca" {
   source = "./modules/grimoire.ca"
 }
 
+# Deprecated - use grimoire_ca_name_servers to disambiguate the domain name.
 output "name_servers" {
   value = "${module.grimoire_ca.name_servers}"
 }
 
+# Deprecated - use grimoire_ca_zone_id to disambiguate the domain name.
 output "zone_id" {
+  value = "${module.grimoire_ca.zone_id}"
+}
+
+output "grimoire_ca_name_servers" {
+  value = "${module.grimoire_ca.name_servers}"
+}
+
+output "grimoire_ca_zone_id" {
   value = "${module.grimoire_ca.zone_id}"
 }
