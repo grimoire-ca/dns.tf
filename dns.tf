2a362c145bd6220b2baf94608a866de8c9c8eafb
Owen Jacobson <owen@grimoire.ca>
2018-12-05 21:31:28 -0500

    Revert changes to support multiple zones.
    It turns out the zones I was going to add lapsed last year, and there's no point in paying for R53 resources for them.

---
+++ dns.tf
@@ -0,0 +1,25 @@
+terraform {
+  backend "s3" {
+    bucket = "terraform.grimoire"
+    key    = "dns.tfstate"
+    region = "ca-central-1"
+  }
+}
+
+provider "aws" {
+  version = "~> 1.11"
+
+  region = "ca-central-1"
+}
+
+resource "aws_route53_zone" "primary" {
+  name = "grimoire.ca"
+}
+
+output "name_servers" {
+  value = "${aws_route53_zone.primary.name_servers}"
+}
+
+output "zone_id" {
+  value = "${aws_route53_zone.primary.zone_id}"
+}
