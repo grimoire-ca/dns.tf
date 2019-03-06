a419c5c5b8ca24e56dbe65508ded149c0c85968d
Owen Jacobson <owen@grimoire.ca>
2019-03-05 20:31:30 -0500

    Remove d entry again. Experiment failed.

--- modules/grimoire.ca/linode.tf
+++ modules/grimoire.ca/linode.tf
@@ -33,11 +33,3 @@ resource "aws_route53_record" "tumblr" {
   type    = "CNAME"
   records = ["${aws_route53_record.nj1_ip4.fqdn}"]
 }
-
-resource "aws_route53_record" "d" {
-  zone_id = "${aws_route53_zone.primary.zone_id}"
-  name    = "d"
-  ttl     = "3600"
-  type    = "CNAME"
-  records = ["${aws_route53_record.nj1_ip4.fqdn}"]
-}
