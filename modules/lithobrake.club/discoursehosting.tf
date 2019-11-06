b4a754a5b007200956d865cc790a4116d4bb5468
Owen Jacobson <owen@grimoire.ca>
2019-11-05 23:40:46 -0500

    Moved lithobrake.club main site from readthedocs to discourse

--- modules/lithobrake.club/discoursehosting.tf
+++ modules/lithobrake.club/discoursehosting.tf
@@ -1,7 +1,23 @@
+data "external" "discoursehosting_dns" {
+  program = ["${path.module}/external/from-dns"]
+
+  query = {
+    host = "lithobrake.discoursehosting.net"
+  }
+}
+
 resource "aws_route53_record" "talk" {
   zone_id = aws_route53_zone.primary.zone_id
   name    = "talk"
   type    = "CNAME"
   ttl     = "60"
   records = ["lithobrake.discoursehosting.net"]
+}
+
+resource "aws_route53_record" "a" {
+  zone_id = aws_route53_zone.primary.zone_id
+  name    = ""
+  ttl     = "60"
+  type    = "A"
+  records = split(" ", data.external.discoursehosting_dns.result["A"])
 }
