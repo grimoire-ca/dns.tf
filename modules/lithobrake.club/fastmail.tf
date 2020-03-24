9aa5f13c0c3c1b94f05e2c9923de7c987b98a5b9
Owen Jacobson <owen@grimoire.ca>
2020-03-23 20:41:57 -0400

    Update legacy syntax.

--- modules/lithobrake.club/fastmail.tf
+++ modules/lithobrake.club/fastmail.tf
@@ -1,6 +1,6 @@
 resource "ns1_record" "mx" {
-  zone   = "${ns1_zone.primary.zone}"
-  domain = "${ns1_zone.primary.zone}"
+  zone   = ns1_zone.primary.zone
+  domain = ns1_zone.primary.zone
   type   = "MX"
   ttl    = 3600
 
@@ -14,8 +14,8 @@ resource "ns1_record" "mx" {
 }
 
 resource "ns1_record" "spf" {
-  zone   = "${ns1_zone.primary.zone}"
-  domain = "${ns1_zone.primary.zone}"
+  zone   = ns1_zone.primary.zone
+  domain = ns1_zone.primary.zone
   type   = "TXT"
   ttl    = 3600
 
