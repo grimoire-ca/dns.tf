f0ba2292718184d5e0ba3ec3620fe9d7fb4fe886
Owen Jacobson <owen@grimoire.ca>
2020-08-10 22:19:13 -0400

    Decommission unreasonent.com.

--- main.tf
+++ main.tf
@@ -2,10 +2,6 @@ module "grimoire_ca" {
   source = "./modules/grimoire.ca"
 }
 
-module "unreasonent_com" {
-  source = "./modules/unreasonent.com"
-}
-
 module "lithobrake_club" {
   source = "./modules/lithobrake.club"
 }
@@ -16,14 +12,6 @@ output "grimoire_ca_name_servers" {
 
 output "grimoire_ca_zone_id" {
   value = module.grimoire_ca.zone_id
-}
-
-output "unreasonent_com_name_servers" {
-  value = module.unreasonent_com.name_servers
-}
-
-output "unreasonent_com_zone_id" {
-  value = module.unreasonent_com.zone_id
 }
 
 output "lithobrake_club_name_servers" {
--- modules/unreasonent.com/main.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_route53_zone" "primary" {
-  name = "unreasonent.com"
-
-  tags = {
-    Project = "dns.tf"
-  }
-}
-
