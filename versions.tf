2b9385958ea6ad42ac873a5ef5321a7f49e7aa5b
Owen Jacobson <owen@grimoire.ca>
2020-11-24 02:01:32 -0500

    Upgrade to Terraform 0.13

--- versions.tf
+++ versions.tf
@@ -1,4 +1,15 @@
 
 terraform {
-  required_version = ">= 0.12"
+  required_version = ">= 0.13"
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+    }
+    external = {
+      source = "hashicorp/external"
+    }
+    ns1 = {
+      source = "ns1-terraform/ns1"
+    }
+  }
 }
